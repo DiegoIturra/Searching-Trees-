2f2604e68c17f9deb07cbea034022d154dfb81bd
Diego Iturra <diturra@dparadig.com>
2022-09-10 17:49:52 -0400

    Se agrega clase RedBlackTree

---
+++ redBlackTree.py
@@ -0,0 +1,13 @@
+from binaryTreeClass import Node, BinaryTreeClass
+
+
+class RedBlackTree(BinaryTreeClass):
+
+	def __init__(self):
+		self.root = None
+
+	def insert(self,value):
+		pass
+
+	def search(self.value):
+		pass
